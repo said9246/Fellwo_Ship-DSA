0b14efc1bab9191e84f8f3fd7f73704afaa3c891
said ahmed <144016832+said9246@users.noreply.github.com>
2025-07-16 00:24:18 +0500

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.10%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If found, find the element just larger and swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the elements after i
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
