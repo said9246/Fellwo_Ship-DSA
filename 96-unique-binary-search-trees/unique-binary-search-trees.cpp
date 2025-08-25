7046b9661d9366f34f272a711727ea3659044722
said ahmed <144016832+said9246@users.noreply.github.com>
2025-08-26 01:31:05 +0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (39.34%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;  // base cases
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root-1] * dp[nodes-root];
+            }
+        }
+        return dp[n];
+    }
+};
