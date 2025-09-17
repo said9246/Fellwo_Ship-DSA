5909159bb885b5b9ebc01034596fb4a3e268b36e
said ahmed <144016832+said9246@users.noreply.github.com>
2025-09-17 08:29:36 +0500

    Time: 1 ms (24.52%) | Memory: 8.5 MB (67.32%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     string makeGood(string s) {
-        string result = "";
-        for (char c : s) {
-            if (!result.empty() && abs(result.back() - c) == 32) {
-                result.pop_back(); // Remove the bad pair
-            } else {
-                result.push_back(c); // Add character to result
+        string result="";
+
+        for(char c:s){
+            if(!result.empty() && abs(result.back()-c)==32 ){
+                result.pop_back();
+            }else{
+                result.push_back(c);
             }
         }
         return result;
+        
     }
 };
