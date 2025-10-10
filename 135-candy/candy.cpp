6370b94e22e548f2e46902acdb0ef8c965a455d0
said ahmed <144016832+said9246@users.noreply.github.com>
2025-10-10 08:00:21 +0500

    Time: 3 ms (26.79%) | Memory: 22.9 MB (65.14%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1); // har bachay ko 1 candy
+
+        // left to right check
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // right to left check
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int total = 0;
+        for (int c : candies) total += c;
+        return total;
+    }
+};
