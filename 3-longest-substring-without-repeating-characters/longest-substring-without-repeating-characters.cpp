96ed95147058d9daf77de2c0c66c3e6ff509b453
said ahmed <144016832+said9246@users.noreply.github.com>
2025-11-15 18:45:40 +0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (83.24%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> lastIndex(256, -1); 
+        int maxLen = 0, start = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            // Agar character repeat ho raha ho
+            if (lastIndex[s[i]] >= start) {
+                start = lastIndex[s[i]] + 1;
+            }
+
+            lastIndex[s[i]] = i;  
+            maxLen = max(maxLen, i - start + 1);
+        }
+        return maxLen;
+    }
+};
