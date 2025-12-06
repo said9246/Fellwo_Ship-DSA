5764115461881af130d2a9fd0bba926db7292211
said ahmed <144016832+said9246@users.noreply.github.com>
2025-12-06 10:19:47 +0500

    Time: 8 ms (25.36%) | Memory: 22.1 MB (80.04%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool validPalindrome(string s) {
+int left = 0, right = s.size() - 1;
+
+    while (left < right) {
+        if (s[left] == s[right]) {
+            left++;
+            right--;
+        } else {
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+        }
+    }
+    return true;
+}
+
+private:
+bool isPalindrome(const string &s, int left, int right) {
+while (left < right) {
+if (s[left] != s[right]) return false;
+left++;
+right--;
+}
+return true;
+}
+};
