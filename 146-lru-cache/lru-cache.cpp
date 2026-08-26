df73aac2f639be89b3657247c722f597ec76124e
said ahmed <144016832+said9246@users.noreply.github.com>
2026-08-25 22:03:49 -0700

    Time: 25 ms (89.45%) | Memory: 50.1 MB (90.79%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,43 +1,47 @@
-#include <unordered_map>
-#include <list>
-using namespace std;
-
 class LRUCache {
 public:
     int capacity;
-    list<pair<int, int>> cache; // {key, value}
-    unordered_map<int, list<pair<int, int>>::iterator> mp; // key -> iterator in list
+
+    list<pair<int, int>> lru;
+    unordered_map<int, list<pair<int, int>>::iterator> mp;
 
     LRUCache(int capacity) {
         this->capacity = capacity;
     }
-    
+
     int get(int key) {
         if (mp.find(key) == mp.end())
-            return -1; // not found
-        
-        // move accessed item to front (most recently used)
-        cache.splice(cache.begin(), cache, mp[key]);
-        return mp[key]->second;
+            return -1;
+
+        auto it = mp[key];
+
+        int value = it->second;
+
+        // Recently used → front
+        lru.erase(it);
+        lru.push_front({key, value});
+
+        mp[key] = lru.begin();
+
+        return value;
     }
-    
+
     void put(int key, int value) {
+        // Key already exists
         if (mp.find(key) != mp.end()) {
-            // update existing key
-            mp[key]->second = value;
-            cache.splice(cache.begin(), cache, mp[key]);
-            return;
+            lru.erase(mp[key]);
         }
-        
-        if (cache.size() == capacity) {
-            // remove least recently used
-            int oldKey = cache.back().first;
-            cache.pop_back();
-            mp.erase(oldKey);
+
+        // Add new/recent key at front
+        lru.push_front({key, value});
+        mp[key] = lru.begin();
+
+        // Capacity exceed
+        if (lru.size() > capacity) {
+            auto last = lru.back();
+
+            mp.erase(last.first);
+            lru.pop_back();
         }
-        
-        // insert new key-value
-        cache.push_front({key, value});
-        mp[key] = cache.begin();
     }
 };
